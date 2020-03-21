47eaeda39372856a8a9066a7edf24b7a7ff9b1ab
HarryPan <harrypan91@gmail.com>
2020-03-21 12:11:04 +0800

    Add support for swift 5

--- TwicketSegmentedControl/Palette.swift
+++ TwicketSegmentedControl/Palette.swift
@@ -13,12 +13,12 @@ struct Palette {
     static let highlightTextColor = UIColor.white
     static let segmentedControlBackgroundColor = Palette.colorFromRGB(237, green: 242, blue: 247, alpha: 0.7)
     static let sliderColor = Palette.colorFromRGB(44, green: 131, blue: 255)
-
+    
     static func colorFromRGB(_ red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1.0) -> UIColor {
         func amount(_ amount: CGFloat, with alpha: CGFloat) -> CGFloat {
             return (1 - alpha) * 255 + alpha * amount
         }
-
+        
         let red = amount(red, with: alpha)/255
         let green = amount(green, with: alpha)/255
         let blue = amount(blue, with: alpha)/255
